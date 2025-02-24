c163064dbc7f016ce0a30bdb993b95c72d788db6
Karim O. <146198118+kmoz000@users.noreply.github.com>
2025-02-24 17:52:26 +0000

    Update syncfiles.sh

--- syncfiles.sh
+++ syncfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RSYNC_PASSWORD='SkyNet@11235813'
+export RSYNC_PASSWORD=''
 
 while true; do
     rsync -avz -oHostKeyAlgorithms=+ssh-rsa --progress -e ./luci-app-ispapp/root/usr/libexec/rpcd/ispapp root@192.168.100.1:/usr/libexec/rpcd/ispapp
