07eadff6f5dd988e946ff13e78ef0b242c14eb68
Karim O. <146198118+kmoz000@users.noreply.github.com>
2025-05-16 16:49:19 +0100

    fix: remove hardcoded RSYNC_PASSWORD for improved security

--- syncfiles.sh
+++ syncfiles.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export RSYNC_PASSWORD='SkyNet@11235813'
 
 while true; do
     rsync -avz -oHostKeyAlgorithms=+ssh-rsa --progress -e ./luci-app-ispapp/root/usr/libexec/rpcd/ispapp root@192.168.100.1:/usr/libexec/rpcd/ispapp
